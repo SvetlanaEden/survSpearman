89527891e6778b839f3707aabbc91c95dca1e6dd
Svetlana Eden <svetlana.eden@vanderbilt.edu>
2020-11-17 10:07:24 -0600

    removed PRS related files

--- R/computePSRs.R
+++
@@ -1,9 +0,0 @@
-###################################################################
-######### compute PSRs for censored data
-###################################################################
-computePSRs = function(time, delta){
-  dataWithKM = myOwnKM(time = time, delta = delta)
-  PSRs = dataWithKM$CDF - dataWithKM$delta*(1 - dataWithKM$CDF_M)
-  list(PSRs = PSRs, KM = dataWithKM)
-}
-
