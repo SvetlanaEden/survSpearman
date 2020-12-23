568d3669cc5ff27620f54b6e3d1572cc98d67a38
Svetlana Eden <svetlana.eden@vanderbilt.edu>
2020-12-23 13:48:17 -0600

    added data123.Rd

--- R/data123.R
+++ R/data123.R
@@ -1,11 +1,8 @@
-#' CCASAnetData
+#' data123
 #' 
-#' Bivariate survival data simulated from Frank's copula family with parameter 4.426265, which is equivalent to 0.6 Spearman's correlation.
-#' In the first 100 rows, the follow-up time for all observations is restricted to 2, and some observations may be censored before the follow-up time.
-#' In rows 101:200, the follow-up time is not restricted, but thirty percent of observations are independently censored.
-#' In rows 201:300, there is no censoring.
+#' Bivariate survival data simulated from Frank's copula family with parameter 4.426265, which is equivalent to Spearman's correlation of 0.6. In the first 100 rows, the follow-up time for all observations is restricted to 2, and some observations may be censored before the follow-up time. In rows 101:200, the follow-up time is not restricted, but approximately thirty percent of observations are independently censored. In rows 201:300, there is no censoring.
 #' 
-#' @format A data frame with 6691 rows and 5 variables:
+#' @format A data frame with 300 rows and 4 variables:
 #' \describe{
 #'   \item{X}{Time to event X.}
 #'   \item{deltaX}{Event indicator for event X (1 - event; 0 - censoring event).}
